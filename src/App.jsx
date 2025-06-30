565ccbcc801d09739b7177b3fc1b085b12982a9b
tantrien295 <tantrien1996@gmail.com>
2025-06-30 16:50:41 +0700

    Fix: import Link from react-router-dom cho dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { getKhachHangCount, getDichVuCount, getNhanVienCount, getLichSuCount } from "./api";
 
 function App() {
